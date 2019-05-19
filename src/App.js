2c8aaaabc53c53f406779d5b3a2c4915bf3d508d
Anna Azizova <annaazizova9@gmail.com>
2019-05-19 21:43:44 +0300

    the first assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    username: "Anna",
+  };
+
+  changeNameHandler = (event) => {
+      this.setState({
+        username: event.target.value,
+      })
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UserInput changed={this.changeNameHandler} username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
       </div>
     );
   }
